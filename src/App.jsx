21a86efd01a118d77df1688f554faf92e66a1de9
Tomoneill98 <oneillt98@gmail.com>
2023-08-03 20:59:43 +0100

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+    <div className="text-4xl text-blue-800 font-bold bg-green-500">Tom O'Neill</div>
+    </>
+  )
+}
+
+export default App
