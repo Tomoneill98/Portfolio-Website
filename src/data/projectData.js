b5197d1ae8d7ce016c77af3636f62a4675a9cb31
Tomoneill98 <oneillt98@gmail.com>
2023-08-13 14:16:38 +0100

    Update margins project cards

--- src/data/projectData.js
+++ src/data/projectData.js
@@ -8,7 +8,7 @@ export const projectData = [
     title: "Love Birds",
     img: LoveBirds,
     description:
-      "Love Birds is a mobile application designed to spice up how users in long distance relationships can stay connected. You can send messages, snaps and games for your partner through an external 'egg' which can only be opened by playing a game/answering a quiz set by your partner. The app was designed as part of a team and was built with React Native, Tailwind, TypeScript and Firebase. ",
+      "Love Birds is a mobile app designed to spice up how users in long distance relationships can stay connected. You can send messages, snaps and games for your partner through an external 'egg' which can only be opened by completing a game/ quiz set by your partner. The app was developed as part of a team and built with React Native, Tailwind, TypeScript and Firebase.",
     github: "https://github.com/Tomoneill98/Final-Project-Love-Birds-App",
     demo: "https://northcoders.com/projects/june-2023/love-birds",
   },
@@ -16,7 +16,7 @@ export const projectData = [
     title: "Readdit News",
     img: Frontend,
     description:
-      "Readdit News is a Reddit-style news articles website in which users can log in to a profile and read or review articles of different categories by liking or posting comments under them, as well as viewing comments from other profiles. Otimistic rendering and data fetching have been implemented to create an engaging user interface. The website was built with React and styled with Tailwind.",
+      "Readdit News is a Reddit-style news article website in which users can log in to a profile, read articles of different categories, like and post comments on articles, as well as viewing comments from other profiles. Otimistic rendering and data fetching were implemented to create an engaging user interface. Readdit was built with React and CSS.",
     github: "https://github.com/Tomoneill98/Frontend-Project-NC-News",
     demo: "https://6479ba39bc81921388643fc7--glowing-malasada-c0125d.netlify.app/",
   },
