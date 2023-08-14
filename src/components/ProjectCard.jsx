6601001f8bfa6e3e2e9809624c7c84e3e9b6818d
Tomoneill98 <oneillt98@gmail.com>
2023-08-14 15:05:09 +0100

    Keep project card size for medium screens

--- src/components/ProjectCard.jsx
+++ src/components/ProjectCard.jsx
@@ -3,13 +3,13 @@ import React from "react";
 
 export const ProjectCard = ({ title, description, github, demo, img }) => {
     return (
-        <li className="mx-4 relative list-none md:ml-16 2xl:ml-0 md:w-[400px] md:h-auto lg:w-[500px] lg:h-auto overflow-hidden " style={{ maxWidth: "600px", borderRadius: "20px", }}>
+        <li className="mx-4 relative list-none md:ml-16 2xl:ml-0 md:h-auto lg:w-[500px] lg:h-auto overflow-hidden " style={{ maxWidth: "600px", borderRadius: "20px", }}>
             <img src={img} className="w-full h-auto object-cover max-h-[300px]" alt={title}  />
 
             <div className="absolute inset-0 flex flex-col rounded-2xl justify-center items-center opacity-0 hover:opacity-100 transition-opacity duration-300 bg-pink-200  dark:bg-darkPurple dark:border-4 dark:border-white">
 
                 <h2 className="text-sm md:text-xl font-semibold font-mono -mt-10">{title}</h2>
-                <p className=" text-xs xl:text-base font-mono text-justify leading-none md:leading normal sm:px-2 px-1 lg:m-4"style={{wordSpacing: '-0.3em'}} >{description}</p>
+                <p className=" text-xs lg:text-sm font-mono sm:text-justify leading-none md:leading-normal px-2 sm:px-6 lg:m-4" >{description}</p>
                 
               <div className="absolute left-0 right-0 bottom-0 flex justify-center md:mt-4">
 
