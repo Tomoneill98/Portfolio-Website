0f2c61abb4f23bf7eb48d6f80b96eb3ed1b76851
Tomoneill98 <oneillt98@gmail.com>
2023-09-22 09:22:32 +0100

    Remove comments

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -23,11 +23,13 @@ export default function Projects() {
     return (
         <section id="projects" className="bg-pastelPink w-full min-h-screen dark:bg-darkPurple dark:text-white flex justify-center items-center">
             <div className="max-w-[1240px] w-full p-4">
+              
                 <div className="mb-4 lg:mb-8">
-         <h1 className="text-6xl font-bold font-mono">Projects</h1>
-          <p className="py-2 text-sm md:text-lg font-mono"> // Here's what I have been working on recently.</p>
+                <h1 className="text-6xl font-bold font-mono">Projects</h1>
+                  <p className="py-2 text-sm md:text-lg font-mono"> // Here's what I have been working on recently.</p>
                 </div>
        
+          {/*Carousel*/}
              <Slider {...carousel} className="mx-5 xl:mx-0">
             {projectData.map((project) => (
             <div key={project.title}>
@@ -43,30 +45,7 @@ export default function Projects() {
         </Slider>
         
             </div>
-
         </section>
     )
 }
 
-
-
-
-    // <div className="max-w-7xl mx-auto p-6">
-    //                 <h1 className="text-6xl font-bold font-mono mb-4">Projects</h1>
-    //                 <p className="text-sm md:text-lg font-mono mb-2 sm:mb-0">// Hover over a project to find out more!</p>
-
-    //             <div className="w-full sm:mt-4">
-    //                 <div className="grid grid-cols-1 gap-6 lg:grid-cols-2 md:gap-8">
-    //                     {projectData.map((project) => (
-    //                         <ProjectCard
-    //                             key={project.title}
-    //                             img={project.img}
-    //                             title={project.title}
-    //                             description={project.description}
-    //                             github={project.github}
-    //                             demo={project.demo}
-    //                         />
-    //                     ))}
-    //                 </div>
-    //             </div>
-    //         </div>
